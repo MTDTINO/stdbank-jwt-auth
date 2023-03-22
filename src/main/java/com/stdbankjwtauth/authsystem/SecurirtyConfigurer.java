f3d49e70a75dc2f65009e72b648fc6ddbe17483c
MTDTINO <103025712+MTDTINO@users.noreply.github.com>
2023-03-23 00:32:05 +0200

    Web Security Configurer

--- src/main/java/com/stdbankjwtauth/authsystem/SecurirtyConfigurer.java
+++ src/main/java/com/stdbankjwtauth/authsystem/SecurirtyConfigurer.java
@@ -1,12 +1,16 @@
 package com.stdbankjwtauth.authsystem;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 
+import com.stdbankjwtauth.authsystem.services.FileBasedUserDetailsService;
+
 @EnableWebSecurity
 public class SecurirtyConfigurer extends WebSecurityConfiguration {
-	//@Override
+	@Autowired
+	
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
 		auth.getDefaultUserDetailsService();
 	}	
